50c47b5a57092bb752d64327ed2a0a821c857180
macedo-developer <macedorenata.developer@gmail.com>
2020-07-27 21:36:27 -0300

    Adding BannerMain, Carousel and Footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from "react";
 
 import Menu from "./components/Menu";
 
+import dados from "./data/dados_iniciais.json";
+
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <Menu />
+
+      <BannerMain
+        videoTitle={dados.categorias[0].videos[0].titulo}
+        url={dados.categorias[0].videos[0].url}
+        videoDescription={"O que é FrontEnd? Descubra aqui"}
+      />
+      {dados.categorias.map((categoria) => (
+        <Carousel category={categoria} />
+      ))}
+
+      <Footer />
     </>
   );
 }
